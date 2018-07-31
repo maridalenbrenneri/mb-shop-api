59a1caf791428c3c46ce87f79eb7bb9749c89b1f
Björn Asplund <bjorn.asplund@norwegian.com>
2018-07-31 21:33:50 +0200

    cleanup

--- src/repositories/db-client.js
+++ src/repositories/db-client.js
@@ -1,28 +1,18 @@
-const { Client } = require('pg/lib');
+import { Client } from 'pg/lib';
 
-var dbclient = (function() {
-    const client = new Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-      });
-      
-    function getStuff() {
-        client.connect();
-    
-        client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-            console.log(JSON.stringify(row));
-        }
-        client.end();
-        });
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+export function getStuff() {
+    client.connect();
+
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
     }
-
-    return {
-        getStuff: getStuff
-    }
-
-})();
-
-
-module.exports = dbclient;
+    client.end();
+    });
+}
