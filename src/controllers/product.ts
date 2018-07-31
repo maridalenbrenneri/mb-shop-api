821721ccc93ce8e45ca2ae85aa0be7ef92c67b42
Björn Asplund <bjorn.asplund@norwegian.com>
2018-07-31 20:35:46 +0200

    using pg js client

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -3,6 +3,7 @@
 // import request from "request";
 import { Response, Request, NextFunction } from "express";
 import logger from "../utils/logger";
+import * as dbclient  from "../repositories/db-client"
 
 /**
  * GET /products/:id
@@ -21,6 +22,8 @@ export let getProduct = (req: Request, res: Response) => {
 export let getAllProducts = (req: Request, res: Response) => {
     var productId = req.params.id;
     var provider = req.get('X-Header-Provider');
+
+    dbclient.getStuff();
 
     logger.info("all products was requested");
 
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,27 @@
 import * as winston from "winston";
 
-const logger = winston.createLogger({
+const winston_logger = winston.createLogger({
     transports: [
         new (winston.transports.Console)({ level: process.env.NODE_ENV === "production" ? "info" : "debug" })
     ]
 });
 
 if (process.env.NODE_ENV !== "production") {
-    logger.debug("Logging initialized at debug level");
+    winston_logger.debug("Logging initialized at debug level");
 }
 
-export default logger;
+class Logger {
+    info = function(message) {
+        winston_logger.info(message);
+    };
+
+    warn = function(message) {
+        winston_logger.warn(message);
+    };
+
+    error = function(message) {
+        winston_logger.error(message);
+    };
+}
+
+export default new Logger();
