e202599cb9a375bfbe327e23046136e410f046fa
bjoda <bjuffe@gmail.com>
2019-02-19 18:02:33 +0100

    Work

--- src/services/cargonizer-service.ts
+++ src/services/cargonizer-service.ts
@@ -1,214 +1,239 @@
-
 export class ShippingType {
-	static readonly standard_private = 1;
-	static readonly standard_business = 2;
+  static readonly standard_private = 1;
+  static readonly standard_business = 2;
 }
 
 export class Consignment {
-	shippingType: number;
-	weight: number;
-	reference: string;
-	customer: {
-		email: string;
-		phone: string;
-		name: string;
-		street1: string;
-		street2: string;
-		zipCode: string;
-		place: string;
-		country: string;
-		contactPerson: string;
-	}
+  shippingType: number;
+  weight: number;
+  reference: string;
+  customer: {
+    email: string;
+    phone: string;
+    name: string;
+    street1: string;
+    street2: string;
+    zipCode: string;
+    place: string;
+    country: string;
+    contactPerson: string;
+  };
 }
 
 export class CargonizerService {
-	private prod_consignment_url = "https://cargonizer.no/consignments.xml";
-	private prod_service_partners_url = "https://cargonizer.no/service_partners.xml";
+  private prod_consignment_url = "https://cargonizer.no/consignments.xml";
+  private prod_service_partners_url =
+    "https://cargonizer.no/service_partners.xml";
 
-	private sandbox_consignment_url = "https://sandbox.cargonizer.no/consignments.xml";
-	private sandbox_service_partners_url = "https://cargonizer.no/service_partners.xml"; // use prod, sandbox doesn't work (read only anyway)
+  private sandbox_consignment_url =
+    "https://sandbox.cargonizer.no/consignments.xml";
+  private sandbox_service_partners_url =
+    "https://cargonizer.no/service_partners.xml"; // use prod, sandbox doesn't work (read only anyway)
 
-	private api_key: string;
-	private sender_id: string;
-	private url: string;
-	private transport_agreement: string;
-	private service_partners_url: string;
+  private api_key: string;
+  private sender_id: string;
+  private url: string;
+  private transport_agreement: string;
+  private service_partners_url: string;
 
+  constructor() {
+    const useSandbox = process.env.CARGONIZER_USE_SANDBOX;
 
-	constructor() {
+    this.url = useSandbox
+      ? this.sandbox_consignment_url
+      : this.prod_consignment_url;
+    this.service_partners_url = useSandbox
+      ? this.sandbox_service_partners_url
+      : this.prod_service_partners_url;
 
-		const useSandbox = process.env.CARGONIZER_USE_SANDBOX;
+    this.api_key = useSandbox
+      ? process.env.CARGONIZER_SANDBOX_API_KEY
+      : process.env.CARGONIZER_API_KEY;
+    this.sender_id = useSandbox
+      ? process.env.CARGONIZER_SANDBOX_SENDER_ID
+      : process.env.CARGONIZER_SENDER_ID;
+    this.transport_agreement = useSandbox
+      ? process.env.CARGONIZER_SANDBOX_TRANSPORT_AGREEMENT
+      : process.env.CARGONIZER_TRANSPORT_AGREEMENT;
+  }
 
-		this.url = useSandbox ? this.sandbox_consignment_url : this.prod_consignment_url;
-		this.service_partners_url = useSandbox ? this.sandbox_service_partners_url : this.prod_service_partners_url;
+  public async requestConsignment(consignment: Consignment) {
+    const xml = await this.createConsignmentXml(consignment);
 
-		this.api_key = useSandbox ? process.env.CARGONIZER_SANDBOX_API_KEY : process.env.CARGONIZER_API_KEY;
-		this.sender_id = useSandbox ? process.env.CARGONIZER_SANDBOX_SENDER_ID : process.env.CARGONIZER_SENDER_ID;
-		this.transport_agreement = useSandbox ? process.env.CARGONIZER_SANDBOX_TRANSPORT_AGREEMENT : process.env.CARGONIZER_TRANSPORT_AGREEMENT;
-	}
+    const result = await this.createConsignment(xml);
 
-	public async requestConsignment(consignment: Consignment) {
+    return result;
+  }
 
-		const xml = await this.createConsignmentXml(consignment);
+  private async createConsignment(xml: string) {
+    let options = {
+      url: this.url,
+      method: "POST",
+      headers: {
+        "X-Cargonizer-Key": this.api_key,
+        "X-Cargonizer-Sender": this.sender_id,
+        "Content-length": xml.length
+      },
+      body: xml
+    };
 
-		const result = await this.createConsignment(xml);
+    return new Promise<any>(function(resolve, reject) {
+      const request = require("request");
 
-		return result;
-	}
+      request(options, function(error: any, response: any) {
+        if (error) {
+          return reject(error);
+        }
 
-	private async createConsignment(xml: string) {
-		let options = {
-			url: this.url,
-			method: "POST",
-			headers: {
-				"X-Cargonizer-Key": this.api_key,
-				"X-Cargonizer-Sender": this.sender_id,
-				"Content-length": xml.length
-			},
-			body: xml
-		};
+        if (response.statusCode != 201) {
+          // Most likely a validation error such as an invalid zip code etc.
+          return reject(response.body);
+        }
 
-		return new Promise<any>(function (resolve, reject) {
+        require("xml2js").parseString(response.body, function(
+          parseError: any,
+          result: any
+        ) {
+          if (parseError) {
+            return reject(parseError);
+          }
 
-			const request = require('request');
+          return resolve(result);
+        });
+      });
+    });
+  }
 
-			request(options, function (error: any, response: any) {
-				if (error) {
-					return reject(error);
-				}
+  private async requestServicePartners($country, $postcode) {
+    const url =
+      this.service_partners_url +
+      "?country=" +
+      $country +
+      "&postcode=" +
+      $postcode;
 
-				if(response.statusCode != 201) {
-					// Most likely a validation error such as an invalid zip code etc.
-					return reject(response.body);
-				}
+    return new Promise<any>(function(resolve, reject) {
+      const request = require("request");
+      request(url, function(error: any, response: { body: any }) {
+        if (error) {
+          return reject(error);
+        }
 
-				require('xml2js').parseString(response.body, function (parseError: any, result: any) {
-					if (parseError) {
-						return reject(parseError);
-					}
+        require("xml2js").parseString(response.body, function(
+          parseError: any,
+          result: any
+        ) {
+          if (parseError) {
+            return reject(parseError);
+          }
 
-					return resolve(result);
-				});
-			});
-		});
-	}
+          const partners = result.results["service-partners"][0];
+          const partner = partners["service-partner"][0];
 
-	private async requestServicePartners($country, $postcode) {
+          let servicePartner = {
+            service_partner_number: partner.number[0],
+            address: {
+              name: partner.name[0],
+              address1: partner.address1[0],
+              address2: partner.address2[0],
+              postcode: partner.postcode[0],
+              city: partner.city[0],
+              country: partner.country[0]
+            }
+          };
 
-		const url = this.service_partners_url + '?country=' + $country + '&postcode=' + $postcode;
+          return resolve(servicePartner);
+        });
+      });
+    });
+  }
 
-		return new Promise<any>(function (resolve, reject) {
+  private async createConsignmentXml(consignment: Consignment) {
+    let xml2js = require("xml2js");
 
-			const request = require('request');
-			request(url, function (error: any, response: { body: any; }) {
-				if (error) {
-					return reject(error);
-				}
+    const weight = consignment.weight / 1000; // cargonizer wants kilogram
 
-				require('xml2js').parseString(response.body, function (parseError: any, result: any) {
-					if(parseError) {
-						return reject(parseError);
-					}
+    const service_partner = await this.requestServicePartners(
+      "NO",
+      consignment.customer.zipCode
+    );
 
-					const partners = result.results['service-partners'][0];
-					const partner = partners['service-partner'][0];
+    const obj = {
+      consignments: {
+        consignment: {
+          $: {
+            transport_agreement: this.transport_agreement
+          },
+          product: this.ShippingTypeToProduct(consignment.shippingType),
+          parts: {
+            consignee: {
+              name: consignment.customer.name,
+              address1: consignment.customer.street1,
+              address2: consignment.customer.street2,
+              postcode: consignment.customer.zipCode,
+              city: consignment.customer.place,
+              country: consignment.customer.country,
+              email: consignment.customer.email
+            },
+            service_partner: {
+              number: service_partner.service_partner_number,
+              name: service_partner.address.name,
+              address1: service_partner.address.address1,
+              address2: service_partner.address.address2,
+              postcode: service_partner.address.zipCode,
+              city: service_partner.address.city,
+              country: service_partner.address.country
+            }
+          },
+          items: {
+            item: {
+              $: {
+                type: "package",
+                amount: 1,
+                weight: weight
+              }
+            }
+          },
+          services: {},
+          references: { consignor: consignment.reference },
+          return_address: {
+            name: "Maridalen Brenneri AS",
+            address1: "Sørbråtveien 36",
+            postcode: "0891",
+            city: "Oslo",
+            country: "NO"
+          }
+        }
+      }
+    };
 
-					let servicePartner = {
-						service_partner_number: partner.number[0],
-						address: {
-							name: partner.name[0],
-							address1: partner.address1[0],
-							address2: partner.address2[0],
-							postcode: partner.postcode[0],
-							city: partner.city[0],
-							country: partner.country[0],
-						}
-					}
+    const builder = new xml2js.Builder({
+      renderOpts: { pretty: false },
+      headless: true
+    });
 
-					return resolve(servicePartner);
-				});
-			});
-		});
-	}
+    return builder.buildObject(obj);
+  }
 
-	private async createConsignmentXml(consignment: Consignment) {
+  private ShippingTypeToProduct(shippingType: number): string {
+    /*
+			"postnord_parcel_letter_mypack" - "PostNord MyPack Home Small"
+			"tg_stykkgods" - "PostNord Groupage"
+			"bring_pose_pa_doren_no_rfid" - "Bring Pose pa doren"
+			"bring_bedr_dor_dor" - "Bring Bedriftspakke"
+		*/
 
-		let xml2js = require('xml2js');
-	
-		const weight = consignment.weight / 1000; // cargonizer wants kilogram
+    const product_business = "bring_bedr_dor_dor";
+    const product_private = "bring_pose_pa_doren_no_rfid";
 
-		const service_partner = await this.requestServicePartners('NO', consignment.customer.zipCode);
+    if (shippingType == ShippingType.standard_business) {
+      return product_business;
+    }
 
-		const obj = {
-			consignments: {
-				consignment: {
-					$: {
-						"transport_agreement":  this.transport_agreement
-					},
-					product: this.ShippingTypeToProduct(consignment.shippingType),
-					parts: {
-						consignee: {
-							name: consignment.customer.name,
-							address1: consignment.customer.street1,
-							address2: consignment.customer.street2,
-							postcode: consignment.customer.zipCode,
-							city: consignment.customer.place,
-							country: consignment.customer.country,
-							email: consignment.customer.email
-						},
-						service_partner: {
-							number: service_partner.service_partner_number,
-							name: service_partner.address.name,
-							address1: service_partner.address.address1,
-							address2: service_partner.address.address2,
-							postcode: service_partner.address.zipCode,
-							city: service_partner.address.city,
-							country: service_partner.address.country
-						},						
-					},
-					items: {
-						item: {
-							$: {
-								"type":  'package',
-								"amount": 1,
-								"weight": weight
-							}
-						}
-					 },
-					services: { },
-					references: { consignor: consignment.reference },
-					return_address: {
-						name: "Maridalen Brenneri AS",
-						address1: "Sørbråtveien 36",
-						postcode: "0891",
-						city: "Oslo",
-						country: "NO"
-					}
-				}
-			}
-		};
+    if (process.env.CARGONIZER_USE_SANDBOX) {
+      return "bring_pa_doren";
+    }
 
-		const builder = new xml2js.Builder({
-			renderOpts: { pretty: false },
-			headless: true
-		});
-
-		return builder.buildObject(obj);
-	}
-
-	private ShippingTypeToProduct(shippingType: number) : string {
-
-		const product_business = "tg_stykkgods"; // "Groupage"
-		const product_private = "postnord_parcel_letter_mypack"; // "MyPack Home Small"
-
-		if(shippingType == ShippingType.standard_business) {
-			return product_business;
-		}
-
-		if (process.env.CARGONIZER_USE_SANDBOX) {
-			return "mypack"; 
-		}
-
-		return product_private;
-	}
+    return product_private;
+  }
 }
--- src/services/woo-service.ts
+++ src/services/woo-service.ts
@@ -1,178 +1,217 @@
-import * as moment from 'moment';
-import * as https from 'https';
+import * as moment from "moment";
+import * as https from "https";
 import { SubscriptionFrequence } from "../constants";
-import { SubscriptionDateHelper } from './subscription-date-helper';
+import { SubscriptionDateHelper } from "./subscription-date-helper";
 
-const WOO_API_BASE_URL = 'https://maridalenbrenneri.no/wp-json/wc/v2/';
+const WOO_API_BASE_URL = "https://maridalenbrenneri.no/wp-json/wc/v2/";
 const GIFT_SUBSCRIPTION_GIFT_ID = 968;
 
 class WooService {
+  getActiveGiftSubscriptions() {
+    let self = this;
+    const url =
+      WOO_API_BASE_URL +
+      "orders?" +
+      process.env.WOO_SECRET_PARAM +
+      "&product=" +
+      GIFT_SUBSCRIPTION_GIFT_ID;
 
-    getActiveGiftSubscriptions() {
-        let self = this;
-        const url = WOO_API_BASE_URL + 'orders?' + process.env.WOO_SECRET_PARAM + '&product=' + GIFT_SUBSCRIPTION_GIFT_ID;
+    return new Promise<Array<any>>(function(resolve, reject) {
+      https
+        .get(url, orderResponse => {
+          if (orderResponse.statusCode == 401) {
+            reject(new Error("Not authorized with Woo"));
+          }
 
-        return new Promise<Array<any>>(function (resolve, reject) {
-
-            https.get(url, (orderResponse) => {
-
-                if (orderResponse.statusCode == 401) {
-                    reject(new Error('Not authorized with Woo'))
-                }
-
-                orderResponse.setEncoding('utf8');
-                let rawData = '';
-                orderResponse.on('data', (chunk) => { rawData += chunk; });
-                orderResponse.on('end', () => {
-                    try {
-                        return resolve(self.filterActiveGiftSubscriptions(JSON.parse(rawData)));
-
-                    } catch (e) {
-                        reject(e)
-                    }
-                });
-
-            }).on('error', (e) => {
-                reject(e);
-            });
-        });
-    }
-
-    resolveLastDeliveryDate(firstDeliveryDate: Date, numberOfMonths: number, frequence: number) {
-        const date = moment(firstDeliveryDate).add(numberOfMonths - 1, 'M');
-        return SubscriptionDateHelper.resolveNextDeliveryDate(date.toDate(), frequence); 
-    }
-
-    private filterActiveGiftSubscriptions(orders: any): Array<any> {
-        const self = this;
-        const today = moment().startOf('day');
-
-        const activeGiftSubscriptions = new Array<any>();
-
-        for (const order of orders) {
-
-            const orderId = order.id;
-            const orderNumber = +order.meta_data.find(data => data.key == '_order_number').value;
-            const orderNote = order.customer_note;
-            const orderDate = order.date_created;
-            const orderCustomerName = order.billing.first_name + ' ' + order.billing.last_name;
-
-            for (const item of order.line_items) {
-                if (item.product_id === GIFT_SUBSCRIPTION_GIFT_ID) {
- 
-                    item.orderId = orderId;
-                    item.orderNumber = orderNumber;
-                    item.orderDate = orderDate;
-                    item.orderNote = orderNote
-                    item.orderCustomerName = orderCustomerName;
-
-                    let dbItem = self.mapFromWooToDbModel(item);
-
-                    if(today <= moment(dbItem.lastDeliveryDate)) {
-
-                        activeGiftSubscriptions.push(dbItem);
-                    }
-                }
+          orderResponse.setEncoding("utf8");
+          let rawData = "";
+          orderResponse.on("data", chunk => {
+            rawData += chunk;
+          });
+          orderResponse.on("end", () => {
+            try {
+              return resolve(
+                self.filterActiveGiftSubscriptions(JSON.parse(rawData))
+              );
+            } catch (e) {
+              reject(e);
             }
-        }
+          });
+        })
+        .on("error", e => {
+          reject(e);
+        });
+    });
+  }
 
-        return activeGiftSubscriptions;
+  resolveLastDeliveryDate(
+    firstDeliveryDate: Date,
+    numberOfMonths: number,
+    frequence: number
+  ) {
+    const date = moment(firstDeliveryDate).add(numberOfMonths - 1, "M");
+    return SubscriptionDateHelper.resolveNextDeliveryDate(
+      date.toDate(),
+      frequence
+    );
+  }
+
+  private filterActiveGiftSubscriptions(orders: any): Array<any> {
+    const self = this;
+    const today = moment().startOf("day");
+
+    const activeGiftSubscriptions = new Array<any>();
+
+    for (const order of orders) {
+      const orderId = order.id;
+      const orderNumber = +order.meta_data.find(
+        data => data.key == "_order_number"
+      ).value;
+      const orderNote = order.customer_note;
+      const orderDate = order.date_created;
+      const orderCustomerName =
+        order.billing.first_name + " " + order.billing.last_name;
+
+      for (const item of order.line_items) {
+        if (item.product_id === GIFT_SUBSCRIPTION_GIFT_ID) {
+          item.orderId = orderId;
+          item.orderNumber = orderNumber;
+          item.orderDate = orderDate;
+          item.orderNote = orderNote;
+          item.orderCustomerName = orderCustomerName;
+
+          let dbItem = self.mapFromWooToDbModel(item);
+
+          if (
+            today >= moment(dbItem.firstDeliveryDate) &&
+            today <= moment(dbItem.lastDeliveryDate)
+          ) {
+            activeGiftSubscriptions.push(dbItem);
+          }
+        }
+      }
     }
 
-    private resolveMetadataValue(meta_data: Array<any>, key: string) {
-        const res = meta_data.find(data => data.key === key);
-        return !res ? null : res.value;
+    return activeGiftSubscriptions;
+  }
+
+  private resolveMetadataValue(meta_data: Array<any>, key: string) {
+    const res = meta_data.find(data => data.key === key);
+    return !res ? null : res.value;
+  }
+
+  private resolveNumberOfMOnths(orderItem: any) {
+    // Id's from Woo Product Variation (Not very robust solution...)
+
+    switch (orderItem.variation_id) {
+      case 998:
+      case 1002:
+      case 1006:
+        return 1;
+      case 999:
+      case 1003:
+      case 1007:
+        return 3;
+      case 1000:
+      case 1004:
+      case 1008:
+      case 1012:
+        return 6;
+      case 1001:
+      case 1005:
+      case 1009:
+      case 1019:
+        return 12;
     }
 
-    private resolveNumberOfMOnths(orderItem: any) {
-
-        // Id's from Woo Product Variation (Not very robust solution...)
-
-        switch(orderItem.variation_id) {
-            case 998:
-            case 1002:
-            case 1006:
-                return 1;
-            case 999:
-            case 1003:
-            case 1007:
-                return 3;
-            case 1000:
-            case 1004:
-            case 1008:
-            case 1012:
-                return 6;    
-            case 1001:
-            case 1005:
-            case 1009:
-            case 1019:
-                return 12;
-        }
-
-        if(orderItem.orderId == 3204) {
-            // Hack for a specific order that has a removed variation id 
-            // todo: Can be removed after 19th feb 2019
-            return 3;
-        }
-
-        throw new Error("Unsupported Product Variation,  order id: " + orderItem.orderId + " ,variation id: " + orderItem.variation_id);
-    } 
-
-    private mapFromWooToDbModel(orderItem: any) {
-
-        const df = this.resolveMetadataValue(orderItem.meta_data, 'levering');
-        const frequence = df && df.includes('Annenhver uke') 
-            ? SubscriptionFrequence.fortnightly
-            : SubscriptionFrequence.monthly;
-
-        const nrOfMonths = this.resolveNumberOfMOnths(orderItem);
-        
-        let startDate = null;
-        let startDateString = this.resolveMetadataValue(orderItem.meta_data, 'abo_start');
-
-        if(!startDateString) {
-            startDate = moment(orderItem.orderDate);
-        } else {
-            const regexp = new RegExp('..\...\.....');
-            startDate = regexp.test(startDateString)
-                ? moment(startDateString, 'DD.MM.YYYY')
-                : moment(startDateString)
-        }
-
-        // Check from day before in case the date is an actual delivery date (then next would have been selected).
-        let firstDeliveryDate = SubscriptionDateHelper.resolveNextDeliveryDate(startDate.add(-1, 'd').toDate(), frequence);
-
-        if(orderItem.orderId == 3214 || orderItem.orderId == 3398) {
-            // Special special... Orders has incorrect date
-            firstDeliveryDate = moment("2019-01-01", "YYYY-MM-DD").toDate();
-        }
-
-        const model = {
-            wooOrderId: orderItem.orderId,
-            wooOrderNumber: orderItem.orderNumber,
-            status: 'n/a',
-            orderDate: orderItem.orderDate,
-            originalFirstDeliveryDate: startDate.toDate(),
-            firstDeliveryDate: firstDeliveryDate,
-            lastDeliveryDate: this.resolveLastDeliveryDate(firstDeliveryDate, nrOfMonths, frequence),
-            frequence: frequence,
-            numberOfMonths: nrOfMonths,
-            quantity: +this.resolveMetadataValue(orderItem.meta_data, 'poser'),
-            customerName: orderItem.orderCustomerName,
-            recipient_name: this.resolveMetadataValue(orderItem.meta_data, 'abo_name'),
-            recipient_email: this.resolveMetadataValue(orderItem.meta_data, 'abo_email'),
-            recipient_address: JSON.stringify({
-                street1: this.resolveMetadataValue(orderItem.meta_data, 'abo_address1'),
-                street2: this.resolveMetadataValue(orderItem.meta_data, 'abo_address2'),
-                zipCode: this.resolveMetadataValue(orderItem.meta_data, 'abo_zip'),
-                place: this.resolveMetadataValue(orderItem.meta_data, 'city')
-            }),
-            message_to_recipient: this.resolveMetadataValue(orderItem.meta_data, 'abo_msg_retriever'),
-            note: orderItem.orderNote
-        }
-
-        return model;
+    if (orderItem.orderId == 3204) {
+      // Hack for a specific order that has a removed variation id
+      // todo: Can be removed after 19th feb 2019
+      return 3;
     }
+
+    throw new Error(
+      "Unsupported Product Variation,  order id: " +
+        orderItem.orderId +
+        " ,variation id: " +
+        orderItem.variation_id
+    );
+  }
+
+  private mapFromWooToDbModel(orderItem: any) {
+    const df = this.resolveMetadataValue(orderItem.meta_data, "levering");
+    const frequence =
+      df && df.includes("Annenhver uke")
+        ? SubscriptionFrequence.fortnightly
+        : SubscriptionFrequence.monthly;
+
+    const nrOfMonths = this.resolveNumberOfMOnths(orderItem);
+
+    let startDate = null;
+    let startDateString = this.resolveMetadataValue(
+      orderItem.meta_data,
+      "abo_start"
+    );
+
+    if (!startDateString) {
+      startDate = moment(orderItem.orderDate);
+    } else {
+      const regexp = new RegExp("..........");
+      startDate = regexp.test(startDateString)
+        ? moment(startDateString, "DD.MM.YYYY")
+        : moment(startDateString);
+    }
+
+    // Check from day before in case the date is an actual delivery date (then next would have been selected).
+    let firstDeliveryDate = SubscriptionDateHelper.resolveNextDeliveryDate(
+      startDate.add(-1, "d").toDate(),
+      frequence
+    );
+
+    if (orderItem.orderId == 3214 || orderItem.orderId == 3398) {
+      // Special special... Orders has incorrect date
+      firstDeliveryDate = moment("2019-01-01", "YYYY-MM-DD").toDate();
+    }
+
+    const model = {
+      wooOrderId: orderItem.orderId,
+      wooOrderNumber: orderItem.orderNumber,
+      status: "n/a",
+      orderDate: orderItem.orderDate,
+      originalFirstDeliveryDate: startDate.toDate(),
+      firstDeliveryDate: firstDeliveryDate,
+      lastDeliveryDate: this.resolveLastDeliveryDate(
+        firstDeliveryDate,
+        nrOfMonths,
+        frequence
+      ),
+      frequence: frequence,
+      numberOfMonths: nrOfMonths,
+      quantity: +this.resolveMetadataValue(orderItem.meta_data, "poser"),
+      customerName: orderItem.orderCustomerName,
+      recipient_name: this.resolveMetadataValue(
+        orderItem.meta_data,
+        "abo_name"
+      ),
+      recipient_email: this.resolveMetadataValue(
+        orderItem.meta_data,
+        "abo_email"
+      ),
+      recipient_address: JSON.stringify({
+        street1: this.resolveMetadataValue(orderItem.meta_data, "abo_address1"),
+        street2: this.resolveMetadataValue(orderItem.meta_data, "abo_address2"),
+        zipCode: this.resolveMetadataValue(orderItem.meta_data, "abo_zip"),
+        place: this.resolveMetadataValue(orderItem.meta_data, "city")
+      }),
+      message_to_recipient: this.resolveMetadataValue(
+        orderItem.meta_data,
+        "abo_msg_retriever"
+      ),
+      note: orderItem.orderNote
+    };
+
+    return model;
+  }
 }
 
 export default new WooService();
